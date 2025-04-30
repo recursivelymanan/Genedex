38c5cb8615802fb586c0b8ea2a93a53ec2464555
Manan Chopra <manan.zchopra@gmail.com>
2025-04-30 11:59:17 -0700

    Fixed duplicate recent entries bug

--- context/RecentQueryContext.tsx
+++ context/RecentQueryContext.tsx
@@ -26,11 +26,16 @@ export const RecentQueriesProvider: React.FC<{ children: React.ReactNode }> = ({
   const [recentQueries, setRecentQueries] = useState<string[]>([]);
 
   const addRecentQuery = (query: string) => {
+    const safeQuery = query.toUpperCase();
     let arr = [...recentQueries];
-    const index: number = arr.indexOf(query);
+    console.log("Array before addition: ", arr);
+    const index: number = arr.indexOf(safeQuery);
+    console.log("Index of query in arr: ", index);
     index !== -1 ? arr.splice(index, 1) : null;
+    console.log("Arr after splicing: ", arr);
     arr.length === MAX_RECENT_QUERIES ? arr.pop() : null;
-    arr.unshift(query.toUpperCase());
+    arr.unshift(safeQuery);
+    console.log("Final arr: ", arr);
     setRecentQueries(arr);
   };
 
