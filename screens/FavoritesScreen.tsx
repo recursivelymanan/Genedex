2388248b1640eb3fe0314c58725ebd8ca07ffd7a
Manan Chopra <m1chopra@ucsd.edu>
2025-05-01 16:12:18 -0700

    styled favorites

--- screens/FavoritesScreen.tsx
+++ screens/FavoritesScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text, View, StyleSheet } from "react-native";
 
 import { useFavoritesContext } from "../context/FavoritesContext";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
@@ -16,13 +16,40 @@ const FavoritesScreen: React.FC<Props> = ({ route }) => {
   return (
     <SafeAreaView>
       <Header title="Favorites" />
-      {favorites.map((string) => (
-        <Text key={string} onPress={() => handleSearch(string)}>
-          {string}
-        </Text>
-      ))}
+      {favorites.length === 0 ? (
+        <View style={styles.favorite}>
+          <Text style={styles.text}>No favorites</Text>
+        </View>
+      ) : (
+        favorites.map((string) => (
+          <View style={styles.favorite}>
+            <Text
+              key={string}
+              style={styles.text}
+              onPress={() => handleSearch(string)}
+            >
+              {string}
+            </Text>
+          </View>
+        ))
+      )}
     </SafeAreaView>
   );
 };
 
 export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+  favorite: {
+    backgroundColor: "#c7d7f0",
+    borderRadius: 10,
+    width: "auto",
+    alignSelf: "center",
+    margin: 5,
+    padding: 10,
+  },
+  text: {
+    fontSize: 25,
+    fontWeight: 500,
+  },
+});
